2333ceee4a79c6e75636d745798ef8cc383c68b3
Den Kasyanov <den@den.ai>
2021-05-19 02:43:06 +0300

    Replace APIViews with GenericViewSet for Movies

--- app/movies/urls.py
+++ app/movies/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path
+from django.conf.urls import url
+from django.urls import include, path
 
-from .views import MovieList, MovieDetail
+from .views import MovieViewSet  #, MovieList, MovieDetail
+
+from rest_framework import routers
+
+
+router = routers.SimpleRouter()
+router.register(r'movies', MovieViewSet)
 
 
 urlpatterns = [
-    path("api/movies/", MovieList.as_view()),
-    path("api/movies/<int:pk>/", MovieDetail.as_view()),
+    # path("api/movies/", MovieList.as_view()),
+    # path("api/movies/<int:pk>/", MovieDetail.as_view()),
+    path('api/', include(router.urls)),
 ]
